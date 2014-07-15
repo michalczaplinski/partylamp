0ed866f0d12681e9a3d049df12cd0c084fc74b71
Michal Czaplinski <mmczaplinski@gmail.com>
2014-07-15 19:54:32 +0200

    working

--- partylamp2/partylamp2.ino
+++ partylamp2/partylamp2.ino
@@ -1,26 +1,31 @@
 int currentValue;
 int maxValue;
 int minValue;
-unsigned long timer;
-int sampleSpan = 300; // Amount in milliseconds to sample data
-int volume; // this roughly goes from 0 to 700
 
 int led1 = 13;
 int led2 = 06;
-int threshold = 100; //Change This
 
-void setup() 
-{
-  pinMode(led1, OUTPUT); 
-  pinMode(led2, OUTPUT);    
-  Serial.begin(9600); 
-    resetValues();
+int left;
+int right;
+
+unsigned long timer;
+int sampleSpan = 1000; // Amount in milliseconds to sample data
+int volume;            // this roughly goes from 0 to 70
+int threshold = 15;     //single digit numbers
+
+void setup() {
+    pinMode(led1, OUTPUT); 
+    pinMode(led2, OUTPUT);    
+    Serial.begin(9600); 
+        resetValues();
 }
 
-void loop() 
-{
+void loop() {
     currentValue = analogRead(A0);
-
+    int pin4state = analogRead(A4);
+    int pin5state = analogRead(A5);
+    int state = checkstate();
+    
     if (currentValue < minValue) {
         minValue = currentValue;
     } 
@@ -32,26 +37,79 @@ void loop()
         volume = maxValue - minValue;
 
         Serial.println(volume);
-
         resetValues();
     }
-    
-     if(volume>threshold){
-    digitalWrite(led1, HIGH); //Turn ON Led
-    digitalWrite(led2, LOW);
-  }  
-  else{
-    digitalWrite(led1, LOW); // Turn OFF Led
-    digitalWrite(led2, HIGH); 
+      
+    if (state == 1) {
+        if(volume > threshold) { 
+            red();
+        }
+        else {
+            green();
+        }
+    }
+          
+    else if (state == 2) {
+        if(volume > threshold) {
+          green();
+        }
+        else {
+          red();
+        }
+    }
+    else {
+        none();
+    }
+}
+
+
+////////////////////////////////////////////////////
+////////////////// MY FUNCTIONS ////////////////////
+
+// check the state of the switch
+int checkstate() {
+  int pin4state = analogRead(A4);
+  int pin5state = analogRead(A5);
+  
+  // off
+  if (pin4state > 0 && pin5state > 0) {
+    return 0; 
+  }
+  // party mode
+  else if (pin4state == 0) {
+    return 1;
+  }
+  // chill mode
+  else if (pin5state == 0) {
+    return 2;
   }
 }
-
-void resetValues()
-{
-    maxValue = 0;
-    minValue = 1024;
-    timer = millis(); 
+    
+// 
+void red() {
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, LOW);
+//  Serial.println("RED"); // for debugging
 }
 
+void green() {
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, HIGH);
+//  Serial.println("GREEN"); // for debugging
+}
+
+// the lamp is "off"
+void none() {
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+//  Serial.println("NONE"); // for debugging
+}
+
+
+void resetValues() {
+    maxValue = 0;
+    minValue = 1024;
+    timer = millis();
+}
 
 
